50b76c7fb1805870c8875e499a4fa0635d154fae
Orlando Ramos <orlandoramosperez26@gmail.com>
2024-11-06 17:00:14 -0600

    Descripcion de los cambios

--- app/Views/user/templates/AuditDetailsTemplate/bodyTemplate.php
+++ app/Views/user/templates/AuditDetailsTemplate/bodyTemplate.php
@@ -1,8 +1,5 @@
 <div class="w3-main">
-    <header class="w3-container w3-top w3-hide-large w3-indigo w3-xlarge w3-padding">
-        <a href="javascript:void(0)" class="w3-button w3-indigo w3-margin-right" onclick="w3_open()">☰</a>
-        <span>Auditoria Por Capas</span>
-    </header>
+
 
     <div class="w3-container" id="contact" style="margin-top: 75px">
         <h2 class="w3-border-bottom w3-border-light-grey w3-padding-16">Mantenimiento Preventivo / Casa de Fuerza - Auditoría por Capas</h2>
